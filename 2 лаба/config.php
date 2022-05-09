2fee6ee0de4249157ff78f9c0779a156092f2b43
eviik <105224125+eviik@users.noreply.github.com>
2022-05-09 17:40:44 +0200

    Add files via upload

---
+++ 2 лаба/config.php
@@ -0,0 +1,7 @@
+﻿<?php
+define("SITE_URL", 'http://' . getenv('SERVER_NAME') . '/');
+define("DIR_ROOT", (empty($_SERVER['SUBDOMAIN_DOCUMENT_ROOT'])?$_SERVER['DOCUMENT_ROOT']:$_SERVER['SUBDOMAIN_DOCUMENT_ROOT']).'/');
+define("DIR_TEMPLATES", DIR_ROOT . 'templates/');
+define("DIR_LIB", DIR_ROOT . 'lib/');
+define("STYLE_URL", SITE_URL . '/style/style.css');
+?>
