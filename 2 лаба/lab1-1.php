29f09c3342f2ac529e4b0feca88ed59192b745eb
eviik <105224125+eviik@users.noreply.github.com>
2022-05-09 17:45:41 +0200

    Delete 2 лаба directory

--- 2 лаба/lab1-1.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-require_once('config.php');
-
-$tpl_vars=array();
-$tpl_file='lab1-1.tmpl';
-require_once('tpl_config.php');
-?>
--- 2 лаба/lab1-5.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-require_once('config.php');
-
-$tpl_vars=array();
-$tpl_file='lab1-5.tmpl';
-require_once('tpl_config.php');
-?>
