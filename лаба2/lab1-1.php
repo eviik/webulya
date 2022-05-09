c0f37a46c9adceedd24fd95697f3a84b5f3385a8
eviik <105224125+eviik@users.noreply.github.com>
2022-05-09 17:47:28 +0200

    Add files via upload

---
+++ лаба2/lab1-1.php
@@ -0,0 +1,7 @@
+﻿<?php
+require_once('config.php');
+
+$tpl_vars=array();
+$tpl_file='lab1-1.tmpl';
+require_once('tpl_config.php');
+?>
---
+++ лаба2/lab1-4.php
@@ -0,0 +1,7 @@
+﻿<?php
+require_once('config.php');
+
+$tpl_vars=array();
+$tpl_file='lab1-4.tmpl';
+require_once('tpl_config.php');
+?>
